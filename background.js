40f4de52b8ca2f9e8b8b9ce05347bd72146a2d4f
Your Name <you@example1.com>
2018-05-22 18:26:17 +0800

    3

--- background.js
+++ background.js
@@ -11,7 +11,7 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'baidu1.com' },
+                pageUrl: { hostEquals: 'baidu.com' },
             }),
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
