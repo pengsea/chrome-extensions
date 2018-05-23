c87168b2060b5e923f7b0143fea1817d063d006b
Your Name <you@example.com>
2018-05-23 11:23:33 +0800

    111

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 let changeColor = document.getElementById('changeColor');
 let triggerDelete = document.getElementById('triggerDelete');
 let getList = document.getElementById('getList');
+let getWeiBoList = document.getElementById('getWeiboList');
+let weiboList = document.getElementById('weiboList');
 
 chrome.storage.sync.get('color', function (data) {
     changeColor.style.backgroundColor = data.color;
@@ -10,7 +12,7 @@ changeColor.onclick = function (element) {
     let color = element.target.value;
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.executeScript(
-            tabs[ 0 ].id,
+            tabs[0].id,
             { code: 'document.body.style.backgroundColor = "' + color + '";' });
     });
 };
@@ -24,15 +26,50 @@ triggerDelete.onclick = function (element) {
     }`;
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.executeScript(
-            tabs[ 0 ].id,
+            tabs[0].id,
             { code: text });
     });
 };
 getList.onclick = function (element) {
-    let text= '.WB_cardwrap.WB_feed_type.S_bg2.WB_feed_like';
+    let text = '.WB_cardwrap.WB_feed_type.S_bg2.WB_feed_like';
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.executeScript(
-            tabs[ 0 ].id,
+            tabs[0].id,
             { code: text });
     });
 };
+
+getWeiBoList.onclick = function () {
+    let text = `let listItemList = document.querySelectorAll('div[action-type="feed_list_item"]');
+        let arr = [];
+        for(let i = 0; i<listItemList.length; i++){
+            let item = listItemList[i];
+            let mid = item.attributes['mid'].value;
+            let contentText = item.querySelector('div[node-type=feed_list_content]').innerText;
+            arr.push({
+                mid,
+                text:contentText
+            })
+        };`;
+    chromeTabs(text);
+}
+
+function draw(data) {
+    let str = "";
+    data.map((item, index) => {
+        str += "<li style='padding-right:50px;'>" + item.text + "<a href='javascript:;' onClick='delWeiBo(" + item.mid + ")'>删除</a></li>";
+    })
+    weiboList.innerHTML = str;
+}
+
+function delWeiBo(mid) {
+    alert(mid);
+}
+
+function chromeTabs(text) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            { code: text });
+    });
+}
